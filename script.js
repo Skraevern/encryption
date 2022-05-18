67d61c5f6f348d4868e01f018a35deb3a22e7560
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-05-18 18:24:16 +0200

    Finished simple encryption

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
 const clearBtn = document.getElementById("clear-btn");
 const encryptionBtn = document.getElementById("encryption-btn");
-let encryption1Input = document.getElementById("encryption-1");
-let encryption2Input = document.getElementById("encryption-2");
-let encryption3Input = document.getElementById("encryption-3");
-let encryption4Input = document.getElementById("encryption-4");
+let encryptionKey = document.getElementById("encryption-key");
 let plainTextInput = document.getElementById("plaintext");
-let encryption1;
-let encryption2;
-let encryption3;
-let encryption4;
-let decryption1 = document.getElementById("decryption-1");
-let decryption2 = document.getElementById("decryption-2");
-let decryption3 = document.getElementById("decryption-3");
-let decryption4 = document.getElementById("decryption-4");
+let decryptionKey = document.getElementById("decryption-key");
 let encryptedTextInput = document.getElementById("encrypted-text");
+let encryptedText = "";
+let encryption;
 
 const clear = () => {
-  encryption1Input.value = "";
-  encryption2Input.value = "";
-  encryption3Input.value = "";
-  encryption4Input.value = "";
+  encryptionKey.value = "";
   plainTextInput.value = "";
-  decryption1.value = "";
-  decryption2.value = "";
-  decryption3.value = "";
-  decryption4.value = "";
   encryptedTextInput.value = "";
+  encryptionString = "";
+  textString = "";
+  encryption = 0;
+  encryptedText = "";
 };
 
-const encypt = () => {
-  encryption1 = encryption1Input.value;
-  encryption2 = encryption2Input.value;
-  encryption3 = encryption3Input.value;
-  encryption4 = encryption4Input.value;
-  text = plainTextInput.value;
-  decryption1.value = encryption1;
-  decryption2.value = encryption2;
-  decryption3.value = encryption3;
-  decryption4.value = encryption4;
+const encrypt = () => {
+  encryptionString = encryptionKey.value;
+  textString = plainTextInput.value;
 
-  console.log(encryption1, text);
+  for (let i = 0; i < textString.length; i++) {
+    encryption = textString[i].charCodeAt(0) + Number(encryptionString);
+    if (encryption > 126) {
+      for (let i = 0; encryption > 126; i++) {
+        encryption -= 95;
+      }
+    }
+    encryptedText += String.fromCharCode(encryption);
+  }
+  encryptedTextInput.value = encryptedText;
 };
 
 clearBtn.onclick = () => clear();
-encryptionBtn.onclick = () => encypt();
+encryptionBtn.onclick = () => encrypt();
