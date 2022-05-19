81ca94f5ed055e08320d752be279614c5cd47d49
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-05-19 21:13:00 +0200

    Put everything inside object

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
-const clearBtn = document.getElementById("clear-btn");
-const encryptionBtn = document.getElementById("encryption-btn");
-const decryptionBtn = document.getElementById("decryption-btn");
-let encryptionKey = document.getElementById("encryption-key");
-let plainTextInput = document.getElementById("plaintext");
-let decryptionKey = document.getElementById("decryption-key");
-let encryptedTextInput = document.getElementById("encrypted-text");
-let encryptedText = "";
-let encryption = 0;
-let addEncryption;
+const encrypt = {
+  clearBtn: document.getElementById("clear-btn"),
+  encryptionBtn: document.getElementById("encryption-btn"),
+  decryptionBtn: document.getElementById("decryption-btn"),
+  encryptionKey: document.getElementById("encryption-key"),
+  plainTextInput: document.getElementById("plaintext"),
+  decryptionKey: document.getElementById("decryption-key"),
+  encryptedTextInput: document.getElementById("encrypted-text"),
+  encryptedText: "",
+  encryption: 0,
+  addEncryption: true,
 
-const clear = () => {
-  encryptionKey.value = "";
-  plainTextInput.value = "";
-  encryptedTextInput.value = "";
-  encryptionString = "";
-  textString = "";
-  clearMemory();
-};
-const clearMemory = () => {
-  encryptedText = "";
-  encryption = 0;
-};
+  clear() {
+    this.encryptionKey.value = "";
+    this.plainTextInput.value = "";
+    this.encryptedTextInput.value = "";
+    this.encryptionString = "";
+    this.textString = "";
+    this.clearMemory();
+  },
+  clearMemory() {
+    this.encryptedText = "";
+    this.encryption = 0;
+  },
 
-const encrypt = () => {
-  if (addEncryption) {
-    encryptionString = encryptionKey.value;
-    textString = plainTextInput.value;
-  } else if (!addEncryption) {
-    encryptionString = decryptionKey.value;
-    textString = encryptedTextInput.value;
-  }
-  for (let i = 0; i < textString.length; i++) {
-    encryption = textString[i].charCodeAt(0);
-    for (let index = 0; index < encryptionString.length; index++) {
-      if (addEncryption) {
-        encryption += Number(encryptionString[index]);
-        if (encryption > 126) {
-          for (let i = 0; encryption > 126; i++) {
-            encryption -= 95;
+  encryptFunction() {
+    if (this.addEncryption) {
+      this.encryptionString = this.encryptionKey.value;
+      this.textString = this.plainTextInput.value;
+    } else if (!this.addEncryption) {
+      this.encryptionString = this.decryptionKey.value;
+      this.textString = this.encryptedTextInput.value;
+    }
+    for (let i = 0; i < this.textString.length; i++) {
+      this.encryption = this.textString[i].charCodeAt(0);
+      for (let index = 0; index < this.encryptionString.length; index++) {
+        if (this.addEncryption) {
+          this.encryption += Number(this.encryptionString[index]);
+          if (this.encryption > 126) {
+            for (let i = 0; this.encryption > 126; i++) {
+              this.encryption -= 95;
+            }
           }
-        }
-      } else if (!addEncryption) {
-        encryption -= Number(encryptionString[index]);
-        if (encryption < 32) {
-          for (let i = 0; encryption < 32; i++) {
-            encryption += 95;
+        } else if (!this.addEncryption) {
+          this.encryption -= Number(this.encryptionString[index]);
+          if (this.encryption < 32) {
+            for (let i = 0; this.encryption < 32; i++) {
+              this.encryption += 95;
+            }
           }
         }
       }
+      this.encryptedText += String.fromCharCode(this.encryption);
     }
-    encryptedText += String.fromCharCode(encryption);
-  }
-  if (addEncryption) {
-    encryptedTextInput.value = encryptedText;
-  } else if (!addEncryption) {
-    plainTextInput.value = encryptedText;
-  }
-  clearMemory();
+    if (this.addEncryption) {
+      this.encryptedTextInput.value = this.encryptedText;
+    } else if (!this.addEncryption) {
+      this.plainTextInput.value = this.encryptedText;
+    }
+    this.clearMemory();
+  },
+  activateBtns() {
+    this.clearBtn.onclick = () => encrypt.clear();
+    this.encryptionBtn.onclick = () => {
+      this.addEncryption = true;
+      this.encryptFunction();
+    };
+    this.decryptionBtn.onclick = () => {
+      this.addEncryption = false;
+      this.encryptFunction();
+    };
+  },
 };
-
-clearBtn.onclick = () => clear();
-encryptionBtn.onclick = () => {
-  addEncryption = true;
-  encrypt();
-};
-decryptionBtn.onclick = () => {
-  addEncryption = false;
-  encrypt();
-};
+window.onload = () => encrypt.activateBtns();
